e2ed4a84828bcb2c2ac6e1b471fbf862cf999f14
krishheii <kittuboy@yahoo.com>
2020-11-08 20:14:35 +0530

    initial code added

---
+++ index.js
@@ -0,0 +1,28 @@
+export class Stack {
+  constructor() {
+    this.mainStack = [];
+  }
+  peek() {
+    return this.mainStack[this.mainStack.length - 1];
+  }
+  push(value) {
+    if (value) {
+      this.mainStack.push(value);
+    }
+  }
+  pop() {
+    if (this.mainStack.length > 0) {
+      this.mainStack.pop();
+    }
+  }
+  showStack() {
+    let str = "";
+    if (this.mainStack.length === 0) {
+      return (str = "Empty Stack");
+    }
+    for (let item of this.mainStack) {
+      str = str + " " + item;
+    }
+    return str;
+  }
+}
